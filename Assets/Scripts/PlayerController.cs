c098600e64a74c424fb059c979777dacb00cd7da
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-10-18 04:39:58 +1100

    Updating player controller
    Temporarily removed the rope. now if the basket reaches the top the movement stops.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private int _playerSpeed = 5;           // Player/fishing-net speed
-    [SerializeField] private Transform net; // Reference to nets transform, make sure its the actual basket
+    [SerializeField] private Transform _net; // Reference to nets transform, make sure its the actual basket
     private bool _isCasting = false;        // Check if the net is being casted, set to false by default in start/ each round?
-                                            // Score
+
+    [SerializeField] private Transform _topOfWater;
+
+    // Score
 
     // Update is called once per frame
     void Update()
@@ -22,6 +26,7 @@ public class PlayerController : MonoBehaviour
 
         //Check if input has been made to cast the net
         //if button is being pressed & casting isnt set to true
+        //eventually make it so the camera needs to be on the jetty view
         if (Input.GetKeyDown(KeyCode.Space) && !_isCasting)
         {
             _isCasting = true;  //set casting to true
@@ -37,14 +42,21 @@ public class PlayerController : MonoBehaviour
         float _inputX = Input.GetAxis("Horizontal"); //input 1
         float _inputY = Input.GetAxis("Vertical");  //input 2
 
+        // Assign top of screen position
+        float _topOfScreen = _topOfWater.position.y;
+
         // Calculate the new postion for the net, based on what is inputed
-        Vector3 newPositon = net.transform.position + new Vector3(_inputX, _inputY, 0) * _playerSpeed * Time.deltaTime;
+        Vector3 newPositon = _net.transform.position + new Vector3(_inputX, _inputY, 0) * _playerSpeed * Time.deltaTime;
 
         // Move the net to the new position
-        net.position = newPositon;
+        _net.position = newPositon;
 
-    //check if the net has reached the top, use an object or collider transform
-
+        //check if the net has reached the top, use an object or collider transform
+        if (_net.transform.position.y >= _topOfScreen)
+        {
+            _isCasting = false;
+            //trigger the transition to jetty view
+        }
     }
 
 
