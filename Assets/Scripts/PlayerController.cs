6e432be2e76abf628962059c69ad7b85a3a414dd
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-10-18 03:47:06 +1100

    Creating pseudo code for player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+        //Player/ fishing net speed
+        //Check if the net is being casted, set to false by default?
+        //Score
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if net is casted
+        //move the net
+
+        //Check if input has been made to cast the net
+        //if button is being pressed & casting isnt set to true
+        //set casting to true
+
+        //Update the UI to display the score
+    }
+
+    // A function to move the net
+        
+        //calculate the new postion for the net, based on what is inputed
+            //input 1
+            //input 2
+        //move the net to the new position
+
+        //check if the net has reached the top
+
+}
