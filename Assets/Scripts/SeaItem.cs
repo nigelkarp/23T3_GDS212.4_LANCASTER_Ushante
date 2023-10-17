b805e10d41bad06068e1da2add34c37506a69967
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-10-18 05:22:52 +1100

    Created sea item script
    Created and applied sea item script to sea item prefab

---
+++ Assets/Scripts/SeaItem.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeaItem : MonoBehaviour
+{
+    [SerializeField] private int points = 1; // Points given on collection, 1 for now to represent each item
+}
