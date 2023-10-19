1e37983640abd62274a744cfb4d1b978e36795a9
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-10-19 16:21:12 +1100

    Created pseudo code start for itemcollection

---
+++ Assets/Scripts/ItemCollection.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollection : MonoBehaviour
+{
+    // Player Score 
+    // List to store collected items
+
+    // Function to collect an item
+        // Check if the iten is not already collected 
+            // Add the item to the collected items list
+            // Update the score based on the item's value
+
+
+    // Function to display score and collected item on the UI 
+        // Update the score display on the screen
+
+        // Update the collected items displayed on the screen
+
+    // Update the UI to reflect the current score and sollected items
+    
+}
